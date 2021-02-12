87c37fb9e34002cd6eac329d2ca31f7181b6e180
Josh Wheeler <joshwheeler33@gmail.com>
2021-02-11 23:31:49 -0700

    Added Simple Offer.

--- src/main/kotlin/io/joshwheeler/main.kt
+++ src/main/kotlin/io/joshwheeler/main.kt
@@ -14,17 +14,42 @@ fun getInvalidFruitArguments(fruitList: List<String>): List<String> {
 }
 
 /**
- * Converts a list of strings to fruits.
+ * Converts a list of fruit to a map of fruit and its occurrences in the list.
  */
-fun toFruit(fruitList: List<String>): List<Fruit> {
-    return fruitList.map(String::toUpperCase).map(Fruit::valueOf)
+fun toFruitMap(fruitList: List<String>): Map<Fruit, Int> {
+    val fruitMap = mutableMapOf<Fruit, Int>()
+    fruitList.map(String::toUpperCase).map(Fruit::valueOf)
+        .forEach { fruit: Fruit ->
+        fruitMap.computeIfPresent(fruit) { _, v -> v + 1}
+        fruitMap.putIfAbsent(fruit, 1)
+    }
+    return fruitMap
 }
 
 /**
- * Calculates the total price of fruit.
+ * Function to calculate count for "buy one, get one free".
  */
-fun calculateTotal(fruitList: List<Fruit>): Double {
-    return fruitList.sumByDouble { fruit: Fruit -> fruit.price }
+fun buyOneGetOneFree(count: Int): Int = (count / 2) + count % 2
+
+/**
+ * Function to calculate count for "buy three for the price of two".
+ */
+fun buyThreeForTwo(count: Int): Int = ((count / 3) * 2) + count % 3
+
+/**
+ * Calculates the total price of fruit, with discounts applied.
+ */
+fun calculateTotal(fruitMap: Map<Fruit, Int>): Double {
+    var total: Double = 0.0;
+    fruitMap.keys.forEach {
+        // TODO: this would be simplified by the command pattern
+        total += when (it) {
+            Fruit.APPLE -> buyOneGetOneFree(fruitMap[it]!!) * it.price
+            Fruit.ORANGE -> buyThreeForTwo(fruitMap[it]!!) * it.price
+            else -> throw IllegalArgumentException("Unexpected fruit $it")
+        }
+    }
+    return total
 }
 
 /**
@@ -42,5 +67,5 @@ fun main(args: Array<String>) {
         exitProcess(1)
     }
 
-    println(calculateTotal(toFruit(args.asList())))
+    println(calculateTotal(toFruitMap(argsList)))
 }
--- src/test/kotlin/io/joshwheeler/MainKtTest.kt
+++ src/test/kotlin/io/joshwheeler/MainKtTest.kt
@@ -1,14 +1,8 @@
 package io.joshwheeler
 
-import assertk.Assert
-import org.junit.jupiter.api.Assertions.*
-
 import assertk.assertThat
 import assertk.assertions.*
 import org.junit.jupiter.api.Test
-import io.joshwheeler.getInvalidFruitArguments
-import io.joshwheeler.toFruit
-import io.joshwheeler.calculateTotal
 import kotlin.IllegalArgumentException
 import kotlin.test.assertFailsWith
 
@@ -37,35 +31,80 @@ internal class MainKtTest {
     }
 
     /**
-     * Tests conversion of string to Fruit.
+     * Tests a conversion of a list of fruit strings to a map.
      */
     @Test
-    fun testToFruit() {
+    fun testToFruitMap() {
         val validApples: List<String> = listOf("apple", "Apple", "APPLE", "aPpLe")
-        val apples: List<Fruit> = toFruit(validApples)
-        assertThat(apples).containsOnly(Fruit.APPLE)
+        val applesMap: Map<Fruit, Int> = toFruitMap(validApples)
+        assertThat(applesMap.keys).containsOnly(Fruit.APPLE)
+        assertThat(applesMap[Fruit.APPLE]).isEqualTo(4)
 
         val validOranges: List<String> = listOf("orange", "Orange", "ORANGE", "OrAnGe")
-        val oranges: List<Fruit> = toFruit(validOranges)
-        assertThat(oranges).containsOnly(Fruit.ORANGE)
+        val orangesMap: Map<Fruit, Int> = toFruitMap(validOranges)
+        assertThat(orangesMap.keys).containsOnly(Fruit.ORANGE)
+        assertThat(orangesMap[Fruit.ORANGE]).isEqualTo(4)
 
         val invalidOranges: List<String> = listOf("oringe")
-        assertFailsWith<IllegalArgumentException> { toFruit(invalidOranges) }
+        assertFailsWith<IllegalArgumentException> { toFruitMap(invalidOranges) }
 
         val invalidApples: List<String> = listOf("apol")
-        assertFailsWith<IllegalArgumentException> { toFruit(invalidApples) }
+        assertFailsWith<IllegalArgumentException> { toFruitMap(invalidApples) }
+
+        val emptyMap: Map<Fruit, Int> = toFruitMap(listOf())
+        assertThat(emptyMap).hasSize(0)
+        assertThat(emptyMap[Fruit.APPLE]).isNull();
+        assertThat(emptyMap[Fruit.ORANGE]).isNull();
+
+        val fruitMap: Map<Fruit, Int> = toFruitMap(validApples + validOranges)
+        assertThat(fruitMap[Fruit.APPLE]).isEqualTo(4)
+        assertThat(fruitMap[Fruit.ORANGE]).isEqualTo(4)
     }
 
     /**
-     * Tests calculation of total of fruits.
+     * Tests calculation of totalling fruits given a list.
      */
     @Test
-    fun testCalculateTotal() {
-        assertThat(calculateTotal(listOf(Fruit.APPLE))).isEqualTo(0.6)
-        assertThat(calculateTotal(listOf(Fruit.ORANGE))).isEqualTo(0.25)
+    fun testCalculateTotalFromMap() {
+        // Single pieces of fruit
+        assertThat(calculateTotal(mapOf(Pair(Fruit.APPLE, 1)))).isEqualTo(Fruit.APPLE.price)
+        assertThat(calculateTotal(mapOf(Pair(Fruit.ORANGE, 1)))).isEqualTo(Fruit.ORANGE.price)
 
+        // Multiple pieces of fruit on the exactly matching deals
         assertThat(calculateTotal(
-            listOf(Fruit.APPLE, Fruit.APPLE, Fruit.ORANGE, Fruit.APPLE)
-        )).isEqualTo(2.05)
+            mapOf(Pair(Fruit.APPLE, 2), Pair(Fruit.ORANGE, 3))
+        )).isEqualTo(Fruit.APPLE.price + Fruit.ORANGE.price * 2)
+
+        // Pieces of fruit with n-1 of deals
+        assertThat(calculateTotal(
+            mapOf(Pair(Fruit.APPLE, 1), Pair(Fruit.ORANGE, 2))
+        )).isEqualTo(Fruit.APPLE.price + Fruit.ORANGE.price * 2)
+
+        // Pieces of fruit with n+1 of deals
+        assertThat(calculateTotal(
+            mapOf(Pair(Fruit.APPLE, 3), Pair(Fruit.ORANGE, 5))
+        )).isEqualTo(Fruit.APPLE.price * 2 + Fruit.ORANGE.price * 4)
+    }
+
+    /**
+     * Tests the "buy one, get one free" function.
+     */
+    @Test
+    fun testBuyOneGetOneFree() {
+        assertThat(buyOneGetOneFree(1)).isEqualTo(1)
+        assertThat(buyOneGetOneFree(2)).isEqualTo(1)
+        assertThat(buyOneGetOneFree(3)).isEqualTo(2)
+    }
+
+    /**
+     * Tests the "buy three for the price of two" function.
+     */
+    @Test
+    fun testBuyThreeForTwo() {
+        assertThat(buyThreeForTwo(1)).isEqualTo(1)
+        assertThat(buyThreeForTwo(2)).isEqualTo(2)
+        assertThat(buyThreeForTwo(3)).isEqualTo(2)
+        assertThat(buyThreeForTwo(4)).isEqualTo(3)
+        assertThat(buyThreeForTwo(5)).isEqualTo(4)
     }
 }
